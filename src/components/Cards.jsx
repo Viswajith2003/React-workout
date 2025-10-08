b322bc00cc2dae7790659644535adec07eccc44d
Viswajith2003 <viswajithviswa715@gmail.com>
2025-10-08 23:49:17 +0530

    counter set using stare

---
+++ src/components/Cards.jsx
@@ -0,0 +1,10 @@
+export default function Cards({ title, content }) {
+  // console.log(props);
+  // const {title,content}=props
+  return (
+    <div>
+      <h1>Card Title : {title}</h1>
+      <p>Card Content : {content}</p>
+    </div>
+  );
+}
---
+++ src/components/Counter.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+
+export default function Counter() {
+  const [counter, setCounter] = useState(0);
+  return (
+    <div>
+      <h1>Counter: {counter}</h1>
+      <button onClick={() => setCounter(counter + 1)}>+</button>
+      <button onClick={() => setCounter(counter - 1)}>-</button>
+    </div>
+  );
+}
